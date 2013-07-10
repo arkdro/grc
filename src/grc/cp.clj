a24e5d9cbd4e8ef46519116c03774e139cc4e91e
Arkadiy Drovosekov <arkdro@gmail.com>
2013-07-10 20:40:05 +0400

    adding some code

--- src/grc/cp.clj
+++ src/grc/cp.clj
@@ -1,5 +1,25 @@
 (ns grc.cp)
 
-(defn calc [data]
+(defn iter-nodes [nodes colors neg-colors flag]
+  (let [
+        [new-constraints new-nodes new-colors new-neg-colors] (set-one-node
+                                                               nodes
+                                                               colors
+                                                               neg-colors)
+        ]
+    )
+  )
+
+(defn calc [[nodes colors] flag]
+  (iter-nodes nodes colors [] flag)
+  )
+
+(defn timed-calc [graph time-limit]
+  (let [flag (atom :go)
+        my-pool (mk-pool)
+        _ (after time-limit #(compare-and-set! flag :go :stop) my-pool)
+        res (calc graph flag)
+        ]
+    res)
   )
 
